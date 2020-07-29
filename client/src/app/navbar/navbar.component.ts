e9bfe0240c80bd75bbdfda496c72888a72f31508
MaolmeoX <arnaud.maurice.pro@gmail.com>
2020-07-29 14:50:03 +0200

    refactor(client): add client to api repository

---
+++ client/src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent implements OnInit {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    constructor() {}
+
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    ngOnInit(): void {}
+}
---
+++ client/src/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+})
+export class SharedModule {}
