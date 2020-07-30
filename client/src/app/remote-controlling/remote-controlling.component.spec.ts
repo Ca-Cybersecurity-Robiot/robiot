12877f4714bfb7af0db56890ada85b8b8f9c1286
Xely <julien_guya@msn.com>
2020-07-30 23:59:34 +0200

    feature(remote-control): Remote controlling page Display of map

--- client/src/app/remote-controlling/remote-controlling.component.spec.ts
+++ client/src/app/remote-controlling/remote-controlling.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RemoteControllingComponent } from './remote-controlling.component';
 
 describe('RemoteControllingComponent', () => {
-  let component: RemoteControllingComponent;
-  let fixture: ComponentFixture<RemoteControllingComponent>;
+    let component: RemoteControllingComponent;
+    let fixture: ComponentFixture<RemoteControllingComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RemoteControllingComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [RemoteControllingComponent],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RemoteControllingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RemoteControllingComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
