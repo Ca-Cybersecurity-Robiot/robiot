35bdc324d5de26c3fae279ea6ee860f4404eeea3
AKA Camille <makaye151@gmail.com>
2020-07-31 16:40:16 +0200

    feature(parseMAp): Ajout de terrain

---
+++ client/src/assets/terrains/terrain.ts
@@ -0,0 +1,16 @@
+export class terrain {
+    name: string
+    height: Number
+    width: Number
+    obstacles: [
+        {
+            x: Number,
+            y: Number,
+        }]
+    trees: [
+        {
+            x: Number,
+            y: Number,
+        }
+    ]
+}
