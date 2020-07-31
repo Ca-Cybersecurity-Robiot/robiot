aca2cd56eac723fbcdda7684371aa3ebb5230482
AKA Camille <makaye151@gmail.com>
2020-07-31 12:40:18 +0200

    feature(parseMAp): Ajout du formulaire et creation de la data

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
     selector: 'app-home',
@@ -7,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
+    alert: boolean = null ;
+    mapReader= new FileReader()  ;
+    mapReader1 = null;
     constructor() {}
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     ngOnInit(): void {}
+    readFile(file: FileList) {
+        const mapFile = file.item(0);
+        const tab = mapFile.name.split('.');
+        if (tab[1] === 'txt') {
+            this.alert = true;
+            mapFile.text().then( data => {
+                this.mapReader1 = data;
+                console.log(this.mapReader1)})
+        }
+        else{
+            this.alert = false ;
+            console.log('File type error');
+        }
+
+    }
 }
